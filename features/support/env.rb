cf2e239d5d463b60aae9b041f5da4aa3007a4286
Felipe <felipealmeidaw3@gmail.com>
2019-01-22 16:24:44 -0200

    Enviroment

--- features/support/env.rb
+++ features/support/env.rb
@@ -5,8 +5,10 @@ require 'cucumber'
 require 'pry'
 require 'site_prism'
 require 'capybara/cucumber'
+require_relative 'page_helper.rb'
 
 # Configurar web-driver do Capybara
+World(PageObjects)
 Capybara.configure do |config|
   config.default_driver = :selenium_chrome
   config.app_host = 'https://automacaocombatista.herokuapp.com'

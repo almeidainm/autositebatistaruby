40fc159307dec13ac8f4f5632474d8ae2d1dd844
Adriano <adrianolima1p@gmail.com>
2019-01-22 10:42:56 -0200

    Steps finalizados

---
+++ features/step_definitions/formulario_steps.rb
@@ -0,0 +1,36 @@
+
+Dado("que o usuario acesse a pagina da automação com batista") do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Quando("clicar em começar automação web") do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Então("vai para pagina home") do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Quando("o usuario clicar no botão Formulário") do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Quando("no botão Criar Usuários do sub-menu que sera exibido") do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Quando("o usuario preenche o formulario com os dados {string}, {string}, {string}, {string}, {string}, {string}, {string} e {string}") do |string, string2, string3, string4, string5, string6, string7, string8|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Quando("clicar no botão {string}") do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Então("é exibida a tela de usuario criado com sucesso com os dados corretos") do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Então("ele exibira a seguinte mensagem de erro {string}") do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end

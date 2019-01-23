0890747b5d42a398a27a0c90f9dd979b75e2175b
Felipe <felipealmeidaw3@gmail.com>
2019-01-23 09:48:40 -0200

    Alterações dos Passos de acesso ao formulario e page_helper

--- features/pages/treinamento_page.rb
+++ features/pages/treinamento_page.rb
@@ -2,8 +2,12 @@ class AcessandoTreinamento < SitePrism::Page
   set_url '/treinamento/home/'
   element :search_field, 'h5.orange-text'
   element :btn_form, :xpath, '//a[contains(text(),"Formulário")]'
-  def btn_form
+  element :criar_user, :xpath, '//a[contains(text(),"Criar Usuários")]'
+  def botao_form
     btn_form.click
   end 
+  def criar_usuario
+    criar_user.click
+  end  
 end
 
--- features/step_definitions/formulario_steps.rb
+++ features/step_definitions/formulario_steps.rb
@@ -14,11 +14,13 @@ Então("vai para pagina home") do
 end
 
 Quando("o usuario clicar no botão Formulário") do
-  pending # Write code here that turns the phrase above into concrete actions
+  home.botao_form
+  sleep(3)
 end
 
 Quando("no botão Criar Usuários do sub-menu que sera exibido") do
-  pending # Write code here that turns the phrase above into concrete actions
+  home.criar_usuario
+  sleep(3)
 end
 
 Quando("o usuario preenche o formulario com os dados {string}, {string}, {string}, {string}, {string}, {string}, {string} e {string}") do |nome, sobrenome, email, endereco, universidade, profissao, genero, idade|

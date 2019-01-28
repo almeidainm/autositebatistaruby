6d2ed93b0472f96ee999a05886e59bb0001084aa
Felipe <felipealmeidaw3@gmail.com>
2019-01-28 13:44:09 -0200

    Criado os metodos para jogar a tabela/array em variaveis  -incomplemto

--- features/step_definitions/formulario_steps.rb
+++ features/step_definitions/formulario_steps.rb
@@ -70,3 +70,11 @@ Dado("no botão Lista de Usuários do sub-menu que sera exibido") do
   listarUsuarios.vetorLista
 end
 
+Dado("verifica a existência dos dados de usuários {string}, {string}, {string}, {string}, {string}, {string}, {string} e {string}") do |string, string2, string3, string4, string5, string6, string7, string8|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Então("retorna para a página home") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
